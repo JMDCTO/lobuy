64c63cd26bedf9882ef724ceed30b8e7b4f10fd1
JMDCTO <jm.dedecke@yahoo.com>
2022-02-25 19:53:24 +0100

    Revert "initial project commit"
    This reverts commit 45163c2cba2876cd561b7f7f6a5a19d6fbc6d8a1.

--- src/models/poolbusiness.js
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-
-import dotenv from 'dotenv';
-
-import { connectionStringBusiness } from '../settings'
-
-dotenv.config();
-
-export const businessPool = new Pool({ connectionStringBusiness });
